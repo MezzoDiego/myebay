56bc5dd419fe6d21ee5a1d4cb307fac041aae5bb
MezzoDiego <diegomezzo90@gmail.com>
2022-11-19 23:15:19 +0100

    modifiche correttive sulla query di ricerca annunci

--- src/main/java/it/prova/myebay/repository/annuncio/CustomAnnuncioRepositoryImpl.java
+++ src/main/java/it/prova/myebay/repository/annuncio/CustomAnnuncioRepositoryImpl.java
@@ -23,10 +23,10 @@ public class CustomAnnuncioRepositoryImpl implements CustomAnnuncioRepository{
 		Map<String, Object> paramaterMap = new HashMap<String, Object>();
 		List<String> whereClauses = new ArrayList<String>();
 
-		StringBuilder queryBuilder = new StringBuilder("select distinct a from Annuncio a left join a.categorie c  where a.id = a.id ");
+		StringBuilder queryBuilder = new StringBuilder("select distinct a from Annuncio a left join a.categorie c  where a.aperto = true ");
 
 		if (StringUtils.isNotEmpty(example.getTestoAnnuncio())) {
-			whereClauses.add(" a.testoAnnuncio  like :testoAnnuncio ");
+			whereClauses.add(" a.testoAnnuncio like :testoAnnuncio ");
 			paramaterMap.put("testoAnnuncio", "%" + example.getTestoAnnuncio() + "%");
 		}
 		if (example.getPrezzo() != null) {
