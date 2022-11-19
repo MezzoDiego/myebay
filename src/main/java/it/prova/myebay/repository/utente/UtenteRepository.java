384172d22d3f8c39d3b831187043115e4c6d2a60
MezzoDiego <diegomezzo90@gmail.com>
2022-11-19 11:45:53 +0100

    setup repository

---
+++ src/main/java/it/prova/myebay/repository/utente/UtenteRepository.java
@@ -0,0 +1,9 @@
+package it.prova.myebay.repository.utente;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.prova.myebay.model.Utente;
+
+public interface UtenteRepository extends CrudRepository<Utente, Long>{
+
+}
